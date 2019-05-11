f7c691a5a8d0cdde55febaeabe7f1348307d611e
Sam Broner <sbro727@gmail.com>
2019-05-11 11:15:08 -0700

    Get weather

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import Jimp from "jimp";
 import * as path from "path";
 import { exec } from "child_process";
+import * as fetch from "node-fetch";
+
+const zipcodes = require("zipcodes");
 
 const config = require("../config.json");
 console.log(config);
@@ -9,11 +12,25 @@ const writeTo = path.resolve(__dirname, "../release/output.jpeg");
 const readFrom = path.resolve(__dirname, "../bin/background.jpg");
 
 async function getWeather() {
+    const long = zipcodes.lookup("98112").longitude;
+    const lat = zipcodes.lookup("98112").latitude;
+    const coordinates = lat + "," + long;
+    console.log(lat + "," + long);
+    // https://api.darksky.net/forecast/c97761ffd9bcc7fb0da90ea426642e68/37.8267,-122.4233
 
+    const weatherWords = await fetch.default("https://api.darksky.net/forecast/" + config.darkSkyKey + "/" + coordinates)
+        .then(res => {
+            return res.json();
+        })
+        .then((response) => {
+            console.log(response.hourly.summary);
+            return response.hourly.summary;
+        });
+    return weatherWords;
 }
 
 async function makeImage(image: Jimp, font: any) {
-    image.print(font, 200, 200, "Now this is actualy is the overlaydddd");
+    image.print(font, 250, 200, await getWeather());
     image
         .write(writeTo);
 
