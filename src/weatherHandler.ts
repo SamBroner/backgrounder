f91af4ec89cb72710ca01c0c2623a4c5c83ec8f0
Sam Broner <sbro727@gmail.com>
2019-05-18 11:33:55 -0700

    organize the code a bit better

---
+++ src/weatherHandler.ts
@@ -0,0 +1,84 @@
+import * as fetch from "node-fetch";
+import { IWeatherResponse } from './IWeatherResponse';
+import * as path from "path";
+
+const zipcodes = require("zipcodes");
+const config = require("../config.json");
+
+export interface IForecast {
+    path: string;
+    description: string;
+}
+
+export async function getWeatherStrings(zipcode: number): Promise<IForecast[]> {
+    const weather = await getWeather(zipcode);
+    const hourly = weather.hourly.data;
+    const forecastStrings: IForecast[] = new Array();
+
+    for (let i = 0; i < 12; i++) {
+        const hour = hourly[i];
+
+        // Set to Unix Time
+        const date = new Date(hour.time*1000);
+
+        let hours = (date.getHours() % 12)
+        hours = (hours === 0 ) ? 12 : hours;
+        const pm = (date.getHours() > 12)
+        const space = (hours < 10);
+        const temp = hour.temperature.toPrecision(3) + String.fromCharCode(176);
+        const rain = (hour.precipProbability * 100) < 10 ? (hour.precipProbability * 100).toPrecision(1) : (hour.precipProbability * 100).toPrecision(2);
+        
+        forecastStrings.push({
+            path: pickWeatherPath(hour.icon),
+            description: `${hours}${space ? "  ": ""}${pm ? "pm" : "am"} | ${temp} | Rain: ${rain}%`
+        });
+    }
+    return forecastStrings;
+}
+
+async function getWeather(zipcode: number): Promise<IWeatherResponse> {
+    const zipcodeInfo = zipcodes.lookup(zipcode);
+    const long = zipcodeInfo.longitude;
+    const lat = zipcodeInfo.latitude;
+    const coordinates = lat + "," + long;
+
+    return await fetch.default("https://api.darksky.net/forecast/" + 
+            config.darkSkyKey + "/" + coordinates)
+        .then(res => {
+            return res.json();
+        });
+}
+
+function pickWeatherPath(icon: string): string {
+
+    switch(icon) {
+        case "clear-day": {
+            return path.resolve(__dirname, "../bin/common/Sun.png");
+        }
+        case "clear-night": {
+            return path.resolve(__dirname, "../bin/common/Moon.png");
+        }
+        case "rain": {
+            return path.resolve(__dirname, "../bin/common/Cloud-Rain.png");
+        }
+        case "sleet":
+        case "snow": {
+            return path.resolve(__dirname, "../bin/common/Cloud-Snow.png");
+        }
+        case "wind": {
+            return path.resolve(__dirname, "../bin/common/Wind.png");
+        }
+        case "fog": {
+            return path.resolve(__dirname, "../bin/common/Cloud-Fog-Sun.png");
+        }
+        case "partly-cloudy-day": {
+            return path.resolve(__dirname, "../bin/common/Cloud-Sun.png");
+        }
+        case "partly-cloudy-night": {
+            return path.resolve(__dirname, "../bin/common/Cloud-Moon.png");
+        }
+        case "cloudy": { // TODO: Fix Image
+            return path.resolve(__dirname, "../bin/common/Cloud-Fog-Sun.png");
+        }
+    }
+}
